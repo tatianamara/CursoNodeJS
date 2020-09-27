6e914a7aab8a6e2ff37efa7d0fc8992f8a2a1830
tatianamara <tatiana.mara@rockcontent.com>
2020-09-27 19:12:13 -0300

    Working on orders methods and clearing the cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => { //this path works like route from React, will recognize every path with "/"
     Product.find() // retrieve all products automatic
@@ -64,7 +65,7 @@ exports.postCart = (req, res, next) => { // only will receive post requests
     }).then(result => {
         console.log(result);
         res.redirect('/cart');
-    });    
+    });
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
@@ -78,24 +79,39 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-    let fetchedCart;
     req.user
-        .addOrder()
+        .populate('cart.items.productId')
+        .execPopulate() // populate do not return a promise, so we need to call .execPopulate() first
+        .then(user => {
+            const products = user.cart.items.map(i => {
+                return { quantity: i.quantity, product: { ...i.productId._doc } };
+            });
+            const order = new Order({
+                user: {
+                    name: req.user.name,
+                    userId: req.user
+                },
+                products: products
+            });
+            return order.save();
+        })
         .then(result => {
+            return req.user.clearCart();            
+        })
+        .then(() => {
             res.redirect('/orders');
         })
         .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-    req.user
-    .getOrders()
-    .then(orders => {
-        res.render('shop/orders', {
-            pageTitle: 'Your Orders',
-            path: '/orders',
-            orders: orders
-        });
-    })
-    .catch(err => console.log(err));
+    Order.find()
+        .then(orders => {
+            res.render('shop/orders', {
+                pageTitle: 'Your Orders',
+                path: '/orders',
+                orders: orders
+            });
+        })
+        .catch(err => console.log(err));
 };
--- models/user.js
+++ models/user.js
@@ -52,83 +52,14 @@ userSchema.methods.removeFromCart = function (productId) {
     return this.save();
 }
 
+userSchema.methods.clearCart = function () {
+    this.cart = { items: [] };
+    return this.save();
+}
+
 module.exports = mongoose.model('User', userSchema);
 
 
-
-// const mongodb = require('mongodb');
-// const getDb = require('../util/database').getdb;
-
-// class User {
-//     constructor(username, email, cart, id) {
-//         this.name = username;
-//         this.email = email;
-//         this.cart = cart;
-//         this._id = id;
-//     }
-
-//     save() {
-//         const db = getDb();
-//         return db.collection('users').insertOne(this);
-//     }
-
-//     addToCart(product) {
-//         const cartProductIndex = this.cart.items.findIndex(cp => {
-//             return cp.productId.toString() === product._id.toString();
-//         });
-//         let newQuantity = 1;
-//         const updatedCartItems = [...this.cart.items];
-
-//         if (cartProductIndex >= 0) {
-//             newQuantity = this.cart.items[cartProductIndex].quantity + 1;
-//             updatedCartItems[cartProductIndex].quantity = newQuantity;
-//         } else {
-//             updatedCartItems.push({ productId: new mongodb.ObjectID(product._id), quantity: newQuantity });
-//         }
-
-//         const updatedCart = {
-//             items: updatedCartItems // create/updating a object with additional propertie - quantity
-//         };
-//         const db = getDb();
-//         return db.collection('users').updateOne(
-//             { _id: new mongodb.ObjectID(this._id) },
-//             { $set: { cart: updatedCart } }
-//         );
-//     }
-
-//     getCart() {
-//         const db = getDb();
-//         const productIds = this.cart.items.map(i => {
-//             return i.productId;
-//         })
-//         return db
-//             .collection('product')
-//             .find({ _id: { $in: productIds } })
-//             .toArray()
-//             .then(products => {
-//                 return products.map(p => {
-//                     return {
-//                         ...p,
-//                         quantity: this.cart.items.find(i => {
-//                             return i.productId.toString() === p._id.toString();
-//                         }).quantity
-//                     };
-//                 });
-//             });
-//     }
-
-//     deleteItemFromCart(productId) {
-//         const updatedCartItems = this.cart.items.filter(item => {
-//             return item.productId.toString() !== productId.toString();
-//         });
-//         const db = getDb();
-//         return db
-//             .collection('users')
-//             .updateOne(
-//                 { _id: new mongodb.ObjectID(this._id) },
-//                 { $set: { cart: { items: updatedCartItems } } }
-//             );
-//     }
 
 //     addOrder() {
 //         const db = getDb();
