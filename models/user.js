a9edbde5e26d6f328f968e7ad7ade2fbafb5efe3
tatianamara <tatiana.mara@rockcontent.com>
2020-09-27 12:26:44 -0300

    Storing Multiple Products in the cart

--- models/user.js
+++ models/user.js
@@ -15,12 +15,22 @@ class User {
     }
 
     addToCart(product) {
-        // const cartProduct = this.cart.items.findIndex(cp => {
-        //     return cp._id === product._id;
-        // });
+        const cartProductIndex = this.cart.items.findIndex(cp => {
+            return cp.productId.toString() === product._id.toString();
+        });
+        let newQuantity = 1;
+        const updatedCartItems = [...this.cart.items];
+
+        if (cartProductIndex >= 0){
+            newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+            updatedCartItems[cartProductIndex].quantity = newQuantity;
+        }else{
+            updatedCartItems.push({productId: new mongodb.ObjectID(product._id), quantity: newQuantity});
+        }
+
         const updatedCart = {
-            items: [{productId: new mongodb.ObjectID(product._id), quantity: 1}]
-        }; //create a object with additional propertie - quantity
+            items: updatedCartItems // create/updating a object with additional propertie - quantity
+        }; 
         const db = getDb();
         return db.collection('users').updateOne(
             {_id: new mongodb.ObjectID(this._id)}, 
