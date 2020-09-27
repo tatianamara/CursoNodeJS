20216dc9ba83a5edc8d7b4fa9df6800fe1dc7c18
tatianamara <tatiana.mara@rockcontent.com>
2020-09-27 12:58:08 -0300

    Adding orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -97,42 +97,22 @@ exports.postCart = (req, res, next) => { // only will receive post requests
 exports.postCartDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
     req.user
-        .deleteItemFromCart(prodId)        
+        .deleteItemFromCart(prodId)
         .then(result => {
             res.redirect('/cart');
         })
         .catch(err => console.log(err));
 };
 
-// exports.postOrder = (req, res, next) => {
-//     let fetchedCart;
-//   req.user
-//     .getCart()
-//     .then(cart => {
-//       fetchedCart = cart;
-//       return cart.getProducts();
-//     })
-//     .then(products => {
-//       return req.user
-//         .createOrder()
-//         .then(order => {
-//           return order.addProducts(
-//             products.map(product => {
-//               product.orderItem = { quantity: product.cartItem.quantity };
-//               return product;
-//             })
-//           );
-//         })
-//         .catch(err => console.log(err));
-//     })
-//     .then(result => {
-//       return fetchedCart.setProducts(null);
-//     })
-//     .then(result => {
-//       res.redirect('/orders');
-//     })
-//     .catch(err => console.log(err));
-// };
+exports.postOrder = (req, res, next) => {
+    let fetchedCart;
+    req.user
+        .addOrder()
+        .then(result => {
+            res.redirect('/orders');
+        })
+        .catch(err => console.log(err));
+};
 
 // exports.getOrders = (req, res, next) => {
 //     req.user
