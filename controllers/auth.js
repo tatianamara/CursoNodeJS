eb7a588fee6fe2d2bdd41bd56a3d3bca1d26e2f2
tatianamara <tatiana.mara@rockcontent.com>
2020-10-01 15:10:15 -0300

    Setup basic validation and presenting error messages

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendGridTransport = require('nodemailer-sendgrid-transport');
+const { validationResult } = require('express-validator/check');
 
 const User = require('../models/user');
 
@@ -74,7 +75,15 @@ exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
+    const errors = validationResult(req);
 
+    if(!errors.isEmpty()){
+        return res.status(422).render('auth/signup', { // indicating that the validation fails
+            path: '/signup',
+            pageTitle: 'Signup',
+            errorMessage: errors.array()[0].msg
+        });; 
+    }
     User.findOne({ email: email })
         .then(userDoc => {
             if (userDoc) {
