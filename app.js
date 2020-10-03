ef07d62eff15ea0085d32cb30b6e4d0fd25af404
tatianamara <tatiana.mara@rockcontent.com>
2020-10-03 12:21:30 -0300

    Downloading files with authentication

--- app.js
+++ app.js
@@ -60,7 +60,9 @@ const { userInfo } = require('os');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')) // you need pass the name of the field that contains binary data
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(
     session({
         secret: 'my secret',
